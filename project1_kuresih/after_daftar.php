c253e2e6714eb1bdc6ab3a175e8219b40a6f1d18
Kuresih <kuresih58@gmail.com>
2018-12-29 12:56:45 +0700

    inisialisasi

---
+++ project1_kuresih/after_daftar.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <style type="text/css">
+    input[type=text], select {
+      width: 100%;
+      padding: 12px 20px;
+      margin: 8px 0;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-sizing: border-box;
+    }
+
+    input[type=submit] {
+      width: 100%;
+      background-color: #4CAF50;
+      color: white;
+      padding: 14px 20px;
+      margin: 8px 0px;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+
+    select {
+      width: 100%;
+      padding: 12px 20px;
+      border: none;
+      border-radius: 4px;
+      background-color: white;
+    }
+
+    input[type=submit]:hover {
+      background-color: #45a049;
+    }
+
+    input[type=text]:focus {
+      background-color: #FAFFBD;
+    }
+
+  </style>
+
+   <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Business Frontpage - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/business-frontpage.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-warning fixed-top">
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://www.gakgaptek.com"><img src="facebook.png"/></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://www.gakgaptek.com"><img src="instagram.png"/></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"><img src="whatsapp.png"/> : 083824976815</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+      &nbsp; &nbsp;
+      <button type="button" class="btn btn-warning">LOGIN</button>
+    </form>
+    </div>
+      </div>
+    </div>
+    </nav>
+    <br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 navbar-expand-lg">
+          <a href="index.html">
+          <img  width="300" height="75" src="logo.png">
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="container">
+          <div class="row black bg-dark">
+        <div class="col-md-4">
+          <a href="#">
+          <h2 align="center" style="font-size: 25px;color: white;">TENTANG KAMI</h2>
+          </div>
+          <div class="col-md-4">
+            <a href=file:///C:/Users/Hirossie%20Aljuni/Desktop/bootstrap-2B/link1.html>
+            <h2 align="center" style="font-size: 25px ;color: white;">PRODUK KAMI</h2>
+            </div>
+            <div class="col-md-4">
+              <a href=file:///C:/Users/Hirossie%20Aljuni/Desktop/bootstrap-2B/link2.html>
+              <h2 align="center" style="font-size: 25px ;color: white;">WORKSHOP KAMI</h2>
+            </div>
+          </div>
+          <br>
+        </a>
+      
+
+      <p align="center" style="font-size: 30px;color: black;">TERIMAKASIH SUDAH MENDAFTAR<br>SILAHKAN LOGIN KEMBALI</br></p><br><br><br>
+     <center><button type="button" class="btn btn-warning" style="width: 200px; height: 60">LOGIN</button></center><br><br><br><br>
+      <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Prosomi 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+</div>
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
---
+++ project1_kuresih/myprofile.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Business Frontpage - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/business-frontpage.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-warning fixed-top">
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://www.gakgaptek.com"><img src="facebook.png"/></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="http://www.gakgaptek.com"><img src="instagram.png"/></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link"><img src="whatsapp.png"/> : 083824976815</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <!-- <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> -->
+      &nbsp; &nbsp;
+       <div class="button">
+        <a href="login.html" class="btn btn-dark">LOGIN</a>
+      </div> 
+    </form>
+    </div>
+      </div>
+    </div>
+    </nav>
+    <br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 navbar-expand-lg">
+          <a href="index.html">
+          <img  width="300" height="75" src="logo.png">
+        </div>
+      </div>
+    </div>
+    <br>
+    <div class="container">
+      <div class="row black bg-dark">    
+        <div class="col-md-4">
+          <a href="aboutus.html">
+          <h2 align="center" style="font-size: 25px;color: white;">TENTANG KAMI</h2>
+          </div>
+          <div class="col-md-4">
+            <a href=produkkami.html>
+            <h2 align="center" style="font-size: 25px ;color: white;">PRODUK KAMI</h2>
+            </div>
+            <div class="col-md-4">
+              <a href=workshop.html>
+              <h2 align="center" style="font-size: 25px ;color: white;">WORKSHOP KAMI</h2>
+            </div>
+          </div>
+          <br>
+        </a>
+
+        <!-- Page Container -->
+<div class="container">
+      <div class="row black bg-white">    
+          <div class="col-sm-4">
+          <div class="card round">
+            <!-- Profile -->
+            <div class="center"><img src="user.png" class="circle" style="height:106px;width:106px" alt="User"></p>
+             <h4 class="w3-center">My Profile</h4>
+          <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white" title="My Account"></a>
+    <img src="user.png" class="w3-circle" style="height:23px;width:23px">Akun Saya
+         <hr>
+         <a href="myprofile.html" class="w3-bar-item w3-button w3-padding-large">Profile</a><br>
+         <a href="ubahpassword.html" class="w3-bar-item w3-button w3-padding-large">Ubah Password</a>
+
+       </div>
+        
+   </div>
+ </div>
+ <div class="col-sm-8">
+        <h3><b>Profilku</b></h3>
+        <p>Email :</p>
+        <?php
+           
+        ?>
+        <p>Nomor Telepon :</p>
+        <p>Nama User :</p>
+        <p>Alamat :</p>
+        <p>Jenis Kelamin :</p>
+        <p>Tanggal Lahir :</p>
+
+      </div>
+           </div>
+
+           </div>
+
+     
+
+        <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Prosomi 2018</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
